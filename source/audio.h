1f77826ad9eadcc52d638fdf45453d998d3dead7
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2021-08-02 21:06:05 -0500

    Add files via upload

---
+++ source/audio.h
@@ -0,0 +1,15 @@
+/****************************************************************************
+ * Snes9x Nintendo Wii/Gamecube Port
+ *
+ * Tantric 2008-2021
+ *
+ * audio.h
+ *
+ * Audio driver
+ * Audio is fixed to 32Khz/16bit/Stereo
+ ***************************************************************************/
+
+void InitAudio ();
+void AudioStart ();
+void SwitchAudioMode(int mode);
+void ShutdownAudio();
